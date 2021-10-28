d6c6768d2096e0a4ba79581d153a7b1767f9302f
David Saavedra <david.d11@hotmail.com>
2021-10-28 00:25:40 -0500

    agregando useEffect al Login en modo pruebas

--- OneDrive/Escritorio/Daviid/Programacion MinTic UdeA/Ciclo 3/Concesionario/src/layouts/PrivateLayout.jsx
+++ OneDrive/Escritorio/Daviid/Programacion MinTic UdeA/Ciclo 3/Concesionario/src/layouts/PrivateLayout.jsx
@@ -1,14 +1,15 @@
 import Sidebar from 'components/Sidebar';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const PrivateLayout = ({ children }) => {
     return (
         <div className='flex w-screen h-screen'>
-            <Sidebar />
-            <main className='flex w-full bg-blue-400 overflow-y-scroll'>
-                {children}
-            </main>
-            
+            <div className='flex flex-nowrap w-full h-full'>
+                <Sidebar />
+                <main className='flex w-full  overflow-y-scroll items-center justify-center'>
+                    {children}
+                </main>                
+            </div>
         </div>
     )
 }
--- OneDrive/Escritorio/Daviid/Programacion MinTic UdeA/Ciclo 3/Concesionario/src/pages/admin/Productos.jsx
+++ OneDrive/Escritorio/Daviid/Programacion MinTic UdeA/Ciclo 3/Concesionario/src/pages/admin/Productos.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Productos = () => {
+    const [nombreProducto, setNombreProducto] = useState('');
+
+    useEffect(() => {
+        console.log('Hola soy un use effect que se ejecuta solo una vez cuando la pagina se renderiza, por que tiene el array de dependencias vacio');
+    }, []);
+
+    useEffect(()=>{
+        console.log('esto es una funcion que se ejecuta cada que cambia el valor de nombreProducto');
+        console.log('El valor de la variable es', nombreProducto);
+    },[nombreProducto]);
+
+    const enviarDatosAlBackEnd=()=>{
+        console.log("Se envio los datos al BackEnd");
+    }
+
     return (
-        <div>
-            Administracion de productos
-        </div>
+        <form className='flex flex-col'>
+            <h2>Administracion de productos</h2>
+            <input onChange={(e)=>{setNombreProducto(e.target.value)}} type="text" placeholder='Nombre del Producto'  />
+            <input onChange={(e)=>{console.log('Marca: ', e.target.value)}} type="text" placeholder='Marca del Producto' />
+            <input onChange={(e)=>{console.log('Modelo: ', e.target.value)}} type="text" placeholder='Modelo del producto' />
+            <button type='button' onClick={enviarDatosAlBackEnd} className='bg-indigo-500 text-white'>Enviar Datos</button>
+
+        </form>
     )
 }
 
